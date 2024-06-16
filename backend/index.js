8fda1eb17c2aad13fc2be691055eee5ee30be463
Shreyas <shreyasgsm25@gmail.com>
2024-06-17 00:50:06 +0530

    addproduct end-point

--- backend/index.js
+++ backend/index.js
@@ -16,15 +16,66 @@ mongoose.connect(
   "mongodb+srv://tester:shreyas@cluster0.ealgmik.mongodb.net/e-commerce"
 );
 
-app.get("/",(req,res)=>{
-    res.send("Express app is running");
+app.get("/", (req, res) => {
+  res.send("Express app is running");
+});
+//Schema for creatings products
+const Product = mongoose.model("Product", {
+  id: {
+    type: Number,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  new_price: {
+    type: Number,
+    required: true,
+  },
+  old_price: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: String,
+    default: Date.now,
+  },
+  avilable: {
+    type: Boolean,
+    default: true,
+  },
+});
+app.post('/addproduct',async(req,res)=>{
+    const product = new Product({
+        id:req.body.id,
+        name:req.body.name,
+        image:req.body.image,
+        category:req.body.category,
+        new_price:req.body.new_price,
+        old_price:req.body.old_price,
+        
+    });
+    console.log(product);
+    await product.save();
+    console.log("Successfully added product");
+    res.json({
+        success:true,
+        name:req.body.name,
+    })
 })
-app.listen(port,(error)=>{
-    if(!error){
-        console.log('Server running on port',+port);
-    }
-    else{
-        console.log("Error"+error);
-    }
-
-})
+app.listen(port, (error) => {
+  if (!error) {
+    console.log("Server running on port", +port);
+  } else {
+    console.log("Error" + error);
+  }
+});
